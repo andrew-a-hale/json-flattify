b2920afc5495035ebbcefcc048fcfa7d06596c3c
Andrew Hale <andrew.hale003@gmail.com>
2025-04-02 23:24:56 +1000

    added a test

--- src/tests/test_parse.py
+++ src/tests/test_parse.py
@@ -14,8 +14,50 @@ def test_parse_jsonl(file: str):
     assert test == expected
 
 
+@pytest.mark.parametrize("file", ["tests/lines-complex.jsonl"])
+def test_parse_jsonl_2(file: str):
+    test = parse_jsonl(file)
+    expected = [
+        {
+            "values.0.a": 1,
+            "values.0.b.a": 1,
+            "values.1.a": 1,
+            "values.1.b.0.values.0": 1,
+            "values.1.b.0.values.1": 2,
+            "values.1.b.0.values.2": 3,
+            "values.1.b.1.values.0": 1,
+            "values.1.b.1.values.1": 2,
+            "values.1.b.1.values.2": 3,
+            "values.2.a": 1,
+            "values.2.b": 2,
+            "values.3.a": 1,
+            "values.3.b": 2,
+            "values.4.a": 1,
+            "values.4.b": 2,
+        },
+        {
+            "values.0.a": 1,
+            "values.1.a": 1,
+            "values.1.b.0.values.0": 1,
+            "values.1.b.0.values.1": 2,
+            "values.1.b.0.values.2": 3,
+            "values.1.b.1.values.0": 1,
+            "values.1.b.1.values.1": 2,
+            "values.1.b.1.values.2": 3,
+            "values.2.a": 1,
+            "values.2.b": 2,
+            "values.3.a": 1,
+            "values.3.b": 2,
+            "values.4.a": 1,
+            "values.4.b": 2,
+        },
+    ]
+    assert test == expected
+
+
 @pytest.mark.parametrize("file", ["tests/simple.json"])
 def test_parse(file: str):
+    test = parse_json(file)
     expected = [
         {
             "values.0.a": 1,
@@ -30,12 +72,12 @@ def test_parse(file: str):
             "values.4.b": 2,
         }
     ]
-    test = parse_json(file)
     assert test == expected
 
 
 @pytest.mark.parametrize("file", ["tests/normal.json"])
 def test_parse_2(file: str):
+    test = parse_json(file)
     expected = [
         {
             "values.0.a": 1,
@@ -50,12 +92,12 @@ def test_parse_2(file: str):
             "values.4.b": 2,
         }
     ]
-    test = parse_json(file)
     assert test == expected
 
 
 @pytest.mark.parametrize("file", ["tests/complex.json"])
 def test_parse_3(file: str):
+    test = parse_json(file)
     expected = [
         {
             "values.0.a": 1,
@@ -75,5 +117,4 @@ def test_parse_3(file: str):
             "values.4.b": 2,
         }
     ]
-    test = parse_json(file)
     assert test == expected
